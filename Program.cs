6d6e1e91db76502c804c2e04e9f7a89ed0b002c9
Bruna de Paula <brulibra03@hotmail.com>
2023-12-30 14:43:03 -0300

    feat sistema de abstração completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "123444444444444", modelo: "Nokia Lumia", imei: "060106", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("IPhone");
+Smartphone iphone = new Iphone(numero: "12", modelo: "Iphone 11", imei: "6", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
